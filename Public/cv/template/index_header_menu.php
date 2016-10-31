515256e7317ab0780943535a6ff5c103761c6ceb
Tomasz Sapletta <tom@sapletta.com>
2016-10-31 11:04:20 +0100

    layout menu

--- Public/cv/template/index_header_menu.php
+++ Public/cv/template/index_header_menu.php
@@ -17,17 +17,13 @@ foreach($languages->value() as $lang) {
 ?>
 <div class='menu'>
     <div class='menu-left'>
-        Lang:
         <?php echo $menu_html; ?>
-
-        <a href="javascript:doit()" title="Shortcut: [Ctrl] + [P]" >Print Page</a> |
-        <a href="javascript:decode()" title="Decode data" class="decode_button" >Decode private data</a>
-
     </div>
 
     <div class='menu-right'>
-<!--        Download:-->
-<!--        <a href="--><?php //echo 'http://'.$config->getHomeUrl(); ?><!--/Public/cv/download/tom.sapletta.com-cv-en-2014-08.pdf" >EN</a> |-->
+        <a href="javascript:decode()" title="Decode data" class="decode_button" >Decode private data</a> |
+        <a href="javascript:doit()" title="Shortcut: [Ctrl] + [P]" >Print Page</a>
+        <!--        <a href="--><?php //echo 'http://'.$config->getHomeUrl(); ?><!--/Public/cv/download/tom.sapletta.com-cv-en-2014-08.pdf" >EN</a> |-->
 <!--        <a href="--><?php //echo 'http://'.$config->getHomeUrl(); ?><!--/Public/cv/download/tom.sapletta.com-cv-de-2014-08.pdf" >DE</a> |-->
 <!--        <a href="--><?php //echo 'http://'.$config->getHomeUrl(); ?><!--/Public/cv/download/tom.sapletta.com-cv-pl-2014-08.pdf" >PL</a>-->
     </div>
